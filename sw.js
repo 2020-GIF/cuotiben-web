b41759dd3be342e6ad2452457cb17233e8b5c9e6
2020-GIF <2840542949@qq.com>
2026-09-04 20:34:57 +0800

    考岸错题本 网页版 (PWA)

---
+++ sw.js
@@ -0,0 +1,13 @@
+﻿const CACHE = "cuotiben-v4";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./app.html"])));
+  self.skipWaiting();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) { const clone = res.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); }
+      return res;
+    }).catch(() => caches.match("./app.html")))
+  );
+});
